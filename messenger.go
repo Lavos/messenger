93556630cedd4b340ea330b306f47d1d2a9a709e
Kristopher Cost <me@kriscost.com>
2013-07-28 23:55:10 -0700

    master: userlists, global channel

--- messenger.go
+++ messenger.go
@@ -2,41 +2,44 @@ package main
 
 import (
 	"code.google.com/p/go.net/websocket"
-	"fmt"
+	"log"
 	"net/http"
+	"runtime"
+	"time"
 )
-
-// user
 
 const (
 	TYPE_STATUS = "status"
-	TYPE_TEXT = "text"
+	TYPE_TEXT   = "text"
 )
 
 type Message struct {
-	MessageType string `json:"messagetype"`
-	Status Status `json:"status,omitempty"`
-	Text string `json:"text,omitempty"`
-	User string `json:"user,omitempty"`
+	MessageType string   `json:"messagetype"`
+	UserList    []string `json:"user_list,omitempty"`
+	RoomName    string   `json:"room_name,omitempty"`
+	Text        string   `json:"text,omitempty"`
+	User        string   `json:"user,omitempty"`
 }
 
-type Status struct {
-	Users int `json:"users,omitempty"`
-}
+// User
 
 type User struct {
 	id        string
 	websocket *websocket.Conn
 	send      chan Message
-	read	  chan Message
-	join	  chan *Room
-	die	  chan bool
-	name	  string
-	room	  *Room
+	read      chan Message
+	join      chan *Room
+	die       chan bool
+	name      string
+	room_name string
+	room      *Room
 }
 
 func (u *User) run() {
 	go u.reader()
+
+	defer log.Print("user run close")
+	defer u.websocket.Close()
 
 	for {
 		select {
@@ -46,22 +49,22 @@ func (u *User) run() {
 			u.room.register <- u
 
 		case <-u.die:
-			fmt.Print("DIE!\n")
+			log.Print("user die.")
 			u.room.unregister <- u
-			break
+			return
 
 		case message := <-u.read:
 			u.room.broadcast <- message
 
 		case message := <-u.send:
+			log.Printf("[%v] got message: %v", u.name, message)
+
 			err := websocket.JSON.Send(u.websocket, message)
 			if err != nil {
-				break
+				return
 			}
 		}
 	}
-
-	u.websocket.Close()
 }
 
 func (u *User) reader() {
@@ -75,8 +78,8 @@ func (u *User) reader() {
 
 		m := Message{
 			MessageType: TYPE_TEXT,
-			Text: content,
-			User: u.name,
+			Text:        content,
+			User:        u.name,
 		}
 
 		u.read <- m
@@ -84,16 +87,20 @@ func (u *User) reader() {
 
 	u.die <- true
 	u.websocket.Close()
+	log.Print("user read close")
 }
 
 // room
 
 type Room struct {
-	id         string
-	users      map[*User]bool
-	broadcast  chan Message
-	register   chan *User
-	unregister chan *User
+	id          string
+	users       map[*User]bool
+	broadcast   chan Message
+	status      chan chan Message
+	register    chan *User
+	unregister  chan *User
+	statusTimer *time.Timer
+	autoclose   bool
 }
 
 func (r *Room) run(h *Hub) {
@@ -101,95 +108,134 @@ func (r *Room) run(h *Hub) {
 		select {
 		case user := <-r.register:
 			r.users[user] = true
-
-			m := Message{
-				MessageType: TYPE_STATUS,
-				Status: Status{
-					Users: len(r.users),
-				},
-			}
-
-			r.SendToUsers(m)
-			fmt.Printf("current users: %v\n", len(r.users))
+			r.SendStatus()
 		case user := <-r.unregister:
-			// fmt.Print("Unregister!\n")
 			delete(r.users, user)
-			fmt.Printf("current users: %v\n", len(r.users))
 
-			if len(r.users) == 0 {
-				fmt.Print("I'm now empty, unregistering.\n")
+			if r.autoclose && len(r.users) == 0 {
+				log.Printf("[%v] I'm now empty, unregistering.\n", r.id)
 				h.unregister <- r
 			} else {
-				m := Message{
-					MessageType: TYPE_STATUS,
-					Status: Status{
-						Users: len(r.users),
-					},
-				}
-
-				r.SendToUsers(m)
+				r.SendStatus()
 			}
 		case message := <-r.broadcast:
 			r.SendToUsers(message)
+		case returnchan := <-r.status:
+			m := Message{
+				MessageType: TYPE_STATUS,
+				UserList:    r.GetUserList(),
+				RoomName:    r.id,
+			}
+
+			log.Printf("requested status: %v", m)
+			returnchan <- m
 		}
 	}
+
+	log.Printf("Room %s close.", r.id)
+}
+
+func (r *Room) GetUserList() []string {
+	list := make([]string, 0, len(r.users))
+
+	for user, _ := range r.users {
+		list = append(list, user.name)
+	}
+
+	return list
+}
+
+func (r *Room) SendStatus() {
+	if r.statusTimer != nil {
+		r.statusTimer.Stop()
+		r.statusTimer = nil
+	}
+
+	r.statusTimer = time.AfterFunc(1*time.Second, func() {
+		list := r.GetUserList()
+
+		m := Message{
+			MessageType: TYPE_STATUS,
+			UserList:    list,
+			RoomName:    r.id,
+		}
+
+		log.Printf("[%v] current users: %v\n", r.id, len(r.users))
+		r.SendToUsers(m)
+
+		h.updates <- m
+	})
 }
 
 func (r *Room) SendToUsers(m Message) {
 	for current_user := range r.users {
-		select {
-		case current_user.send <- m:
-		default:
-		}
+		current_user.send <- m
 	}
 }
 
 // hub
 
 type Hub struct {
-	rooms	   map[string]*Room
+	rooms      map[string]*Room
 	unregister chan *Room
-	join	   chan *RoomRequest
+	join       chan *User
+	updates    chan Message
 }
 
 func (h *Hub) Run() {
+	global := h.CreateRoom("global", false)
+
 	for {
 		select {
 		case room := <-h.unregister:
 			delete(h.rooms, room.id)
-			// fmt.Printf("current rooms: %v\n", h.rooms)
 
-		case request := <-h.join:
-			room := h.rooms[request.name]
+		case user := <-h.join:
+			room := h.rooms[user.room_name]
 			if room == nil {
-				room = &Room{
-					id:         request.name,
-					users:      make(map[*User]bool),
-					broadcast:  make(chan Message),
-					register:   make(chan *User),
-					unregister: make(chan *User),
-				}
-
-				h.rooms[room.id] = room
-				go room.run(h)
+				room = h.CreateRoom(user.room_name, true)
 			}
 
-			// fmt.Printf("current rooms: %v\n", h.rooms)
-			request.booking <- room
+			user.join <- room
+			if user.room_name == "global" {
+				go h.GetCurrentStatus(user.send)
+			}
+
+		case message := <-h.updates:
+			global.broadcast <- message
 		}
 	}
 }
 
-
-var h = Hub{
-	rooms: make(map[string]*Room),
-	unregister: make(chan *Room),
-	join: make(chan *RoomRequest),
+func (h *Hub) GetCurrentStatus(returnchan chan Message) {
+	for _, reg_room := range h.rooms {
+		reg_room.status <- returnchan
+	}
 }
 
-type RoomRequest struct {
-	name	string
-	booking	chan *Room
+func (h *Hub) CreateRoom(name string, autoclose bool) *Room {
+	room := &Room{
+		id:         name,
+		users:      make(map[*User]bool),
+		broadcast:  make(chan Message),
+		register:   make(chan *User),
+		unregister: make(chan *User),
+		status:     make(chan chan Message),
+		autoclose:  autoclose,
+	}
+
+	log.Printf("[hub] created room: %v", room.id)
+
+	h.rooms[room.id] = room
+	go room.run(h)
+	return room
+}
+
+var h = Hub{
+	rooms:      make(map[string]*Room),
+	unregister: make(chan *Room),
+	join:       make(chan *User),
+	updates:    make(chan Message),
 }
 
 func DoorMan(ws *websocket.Conn) {
@@ -205,19 +251,17 @@ func DoorMan(ws *websocket.Conn) {
 		websocket: ws,
 		send:      make(chan Message),
 		read:      make(chan Message),
-		die:	   make(chan bool),
+		die:       make(chan bool),
 		join:      make(chan *Room),
-		name: user_name,
+		name:      user_name,
+		room_name: room_name,
 	}
 
-	request := &RoomRequest{
-		name: room_name,
-		booking: user.join,
-	}
-
-	h.join <- request
+	h.join <- user
 
 	user.run() // blocks until websocket is closed
+
+	log.Print("DoorMan close")
 }
 
 func Root(c http.ResponseWriter, req *http.Request) {
@@ -225,14 +269,15 @@ func Root(c http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
 	go h.Run()
 	http.HandleFunc("/", Root)
 	http.Handle("/room", websocket.Handler(DoorMan))
 
-	fmt.Print("Started Server.\n")
+	log.Print("Started Server.")
 
 	if err := http.ListenAndServe(":12345", nil); err != nil {
 		panic("ListenAndServe: " + err.Error())
 	}
-
 }
